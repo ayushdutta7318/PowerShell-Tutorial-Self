403a88b7a7fb936c9643f162a6750428845101a3
ayushdutta7318 <ayushdutta7318@gmail.com>
2025-12-01 01:17:16 +0530

    Add files via upload

--- Lec-4.ps1
+++ Lec-4.ps1
@@ -66,3 +66,63 @@ function Restart-ServiceHealth {
 }
 
 "BITS", "ayush" | Restart-ServiceHealth;
+
+# FUNCTIONS WITH MULTI PARAMETERS:
+
+function Set-ServiceState{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ServiceName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Start","Stop","Restart")]
+        [string]$Action
+    )
+
+    try{
+        $Service = Get-Service -Name $ServiceName -ErrorAction Stop;
+
+        switch ($Action) {
+            "Start"{
+                if ($Service.Status -eq "Running"){
+                    Write-Host "$Service is already Running" -ForegroundColor Yellow;
+                }else{
+                    Start-Service -InputObject $Service;
+                    Write-Host "Started Service $ServiceName" -ForegroundColor Green; 
+                }
+            }
+
+           "Stop"{
+            if($Service.Status -eq "Stopped"){
+                Write-Host "Service $ServiceName is already stopped" -ForegroundColor Yellow;
+            }else{
+                Stop-Service -InputObject $Service;
+                Write-Host "Service $ServiceName has been stopped" -ForegroundColor Green;
+            }
+           }
+
+           "Restart"{
+            if($Service.Status -eq "Running"){
+                Write-Host "Service is already running, need not to restart" -ForegroundColor Yellow;
+            } 
+
+            Restart-Service -InputObject $Service -Force;
+            Write-Host "Service $ServiceName restarted" -ForegroundColor Green;
+           }
+        }
+
+        $Service.Refresh();
+        return $Service.Status;
+    }catch{
+        Write-Host "$Action cannot be performed on $ServiceName : $($_.Exception.Message)" -ForegroundColor Red;
+
+        return $null;
+    }
+}
+
+Set-ServiceState -ServiceName "AnyDesk" -Action Stop;
+Set-ServiceState -ServiceName "ALG" -Action Start;
+
+
+
